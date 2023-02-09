c82494f9ab216000b08ee37cdc7cc2d0d9735136
Sanjib Kumar Sen <sksenonline@gmail.com>
2023-02-10 05:47:31 +0600

    Fixed CSV Error

--- trial/main.py
+++ trial/main.py
@@ -22,17 +22,21 @@ from club import Club
 csv_input = open('trial/resources/residentadvisor_root_db_rows.csv', mode='r')
 csv_output = open('trial/resources/output.csv', mode='w')
 csv_reader = csv.DictReader(csv_input)
-writer = csv.DictWriter(csv_output, fieldnames=Club.__dict__)
-writer.writeheader()
+
+
+list_of_clubs = []
 for row in csv_reader:
     id = row["id"]
     venue_name = row["venue_name"]
     owner_url = row["owner_url"]
     location = row["location"]
     print("Currently processing", id)
-
     club = Club(id, venue_name, owner_url, location)
-    writer.writerow(club.__dict__)
+    list_of_clubs.append(club)
 
+writer = csv.DictWriter(
+    csv_output, fieldnames=list_of_clubs[0].__dict__.keys())
+writer.writeheader()
+writer.writerows(list_of_clubs)
 csv_input.close()
 csv_output.close()
